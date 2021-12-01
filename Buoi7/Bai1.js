e70882a6173a27ecaadc6353151033174b8cea96
Đặng Xuân Hoàng <dxh4402@gmail.com>
2021-12-01 08:13:50 +0700

    Update BTVN

--- Buoi7/Bai1.js
+++ Buoi7/Bai1.js
@@ -1,27 +1,42 @@
-//Case 1
+    //Case 1
+//   const nums = [1, 3, 5, 7, 1, 2, 3, 4, 5, 6];
+
+    //Case 2
+//    const nums1 = [1, 1, 1, 10, 23, 2, 26, 26];
+
+    //Case 3
+//    const nums2 = [1000, 2000, 1000, 2000, 10000, 22222];
+
+//    function countNumber(a) {
+//        a.sort();
+//        let count = 1;
+//        for(let i = 0;i<a.length; i++){
+//            if(a[i] == a[i+1])
+//            count++;
+//            else {
+ //               console.log(`phan tu ${a[i]} xuat hien ${count} lan`);
+//                count = 1;
+//            }
+ //       }
+//    }
+ //   console.log("Case 1");
+ //   countNumber(nums);
+ //   console.log("Case 2");
+ //   countNumber(nums1);
+ //   console.log("Case 3");
+ //   countNumber(nums2);
+
+//Anh Duong chua
 const nums = [1, 3, 5, 7, 1, 2, 3, 4, 5, 6];
-
-//Case 2
 const nums1 = [1, 1, 1, 10, 23, 2, 26, 26];
-
-//Case 3
 const nums2 = [1000, 2000, 1000, 2000, 10000, 22222];
 
-function countNumber(a) {
-    a.sort();
-    let count = 1;
-    for(let i = 0;i<a.length; i++){
-        if(a[i] == a[i+1])
-        count++;
-        else {
-            console.log(`phan tu ${a[i]} xuat hien ${count} lan`);
-            count = 1;
-        }
-    }
-}
-console.log("Case 1");
-countNumber(nums);
-console.log("Case 2");
-countNumber(nums1);
-console.log("Case 3");
-countNumber(nums2);
+const count = (nums) => {
+    let map = {};
+    for (const num of nums) map[num] = map.hasOwnProperty(num) ? ++map[num] : 1;
+    return map;
+};
+
+console.log(count(nums));
+console.log(count(nums1));
+console.log(count(nums2));
